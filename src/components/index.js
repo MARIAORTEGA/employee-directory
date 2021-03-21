5f2983192a40c1a1d2ba9f85bd44ed2b784acb0c
Maria Ortega <mariaortegapb@gmail.com>
2021-03-21 15:12:40 -0400

    added api

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,41 @@
 import React, { useState } from "react";
 import "./style.css";
-
+import API from "https://randomuser.me/api/"
+;
 function App() {
   const [developerState, setDeveloperState] = useState({
-    image: "",
+    picture: "",
     username: "",
-    email: "",
+    gender: "",
   });
+
+  loadUsers = () => {
+    API.getRandom()
+    .then(res =>
+      this.setState({
+        picture: picture
+      })
+    )
+    .catch(err => console.log(err));
+};
+  }
+
+return (
+        <div className="card">
+          <div>
+            picture: {developerState.image}
+          </div>
+          <div>
+            Username: {developerState.username}
+          </div>
+          <div>
+            gender: {developerState.gender}
+          </div>
+          <div className="card">
+        <card picture={() => setDeveloperState({ ...developerState })} />
+        </div>
+        </div>
+      );
 }
 
-export default Directory;
+export default App;
