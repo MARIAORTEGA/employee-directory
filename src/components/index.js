e67fd8ee2a82fea9ae031d64a74a9a0cfec9d9f8
Maria Ortega <mariaortegapb@gmail.com>
2021-03-21 14:31:59 -0400

    working on state

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./style.css";
+
+function App() {
+  const [developerState, setDeveloperState] = useState({
+    image: "",
+    username: "",
+    email: "",
+  });
+}
+
+export default Directory;
